cb4ab733cfa6eb1f94c90a504fbfee959977fb52
Foolishme <2324532557@qq.com>
2019-03-03 22:19:16 +0800

    2019.3.3

--- UESTC 2019 WinterTraning/UESTC 2019 Winter Training #7 Group/Advanced Data Structure/A/sol.cpp
+++ UESTC 2019 WinterTraning/UESTC 2019 Winter Training #7 Group/Advanced Data Structure/A/sol.cpp
@@ -5,37 +5,61 @@
 #define mod 1000000007
 #define pb push_back
 #define STREAM_FAST ios::sync_with_stdio(false)
-#define INF 1e9
 
 using namespace std;
 const int maxn = 1e5 + 7;
+const int INF = 1e9;
 struct node{int u, v, w, next;}e[maxn << 1];
-int n; 
-int tot, head[maxn];
+int head[maxn], tot, n;
 void add(int u, int v, int w)
 {
     ++tot;
-    e[tot].u = u; e[tot].v = v; e[tot].w = w; e[tot].next = head[u];
-    head[u] = tot;
+    e[tot].u = u; e[tot].v = v; e[tot].w = w;
+    e[tot].next = head[u]; head[u] = tot;
 }
-int f[maxn], sz[maxn], son[maxn], id[maxn], rnk[maxn], dep[maxn], top[maxn], val[maxn];
-
+int son[maxn], father[maxn], id[maxn], sz[maxn], rnk[maxn], dep[maxn], top[maxn], val[maxn];
+int tim;
+void dfs1(int u, int fa, int deep)
+{
+    son[u] = 0; father[u] = fa; dep[u] = deep;
+    sz[u] = 1;
+    for (int i = head[u]; i; i = e[i].next)
+    {
+        int v = e[i].v;
+        if (v == father[u]) continue;
+        dfs1(v, u, deep + 1);
+        sz[u] += sz[v];
+        if (sz[v] > sz[son[u]]) son[u] = v;
+    }
+}
+void dfs2(int u, int tp)
+{
+    ++tim; id[u] = tim; rnk[tim] = u;
+    top[u] = tp;
+    if (son[u]) dfs2(son[u], tp);
+    for (int i = head[u]; i; i = e[i].next)
+    {
+        int v = e[i].v;
+        if (v == father[u] || v == son[u]) continue;
+        dfs2(v, v);
+    }
+}
 int T[maxn << 2];
 void pushup(int rt) {T[rt] = max(T[rt << 1], T[rt << 1 | 1]);}
 void build(int rt, int l, int r)
 {
-    if (l == r) {T[rt] = val[l]; return;}
+    if (l == r) {T[rt] = val[rnk[l]]; return;}
     int mid = (l + r) >> 1;
     build(rt << 1, l, mid);
     build(rt << 1 | 1, mid + 1, r);
     pushup(rt);
 }
-void update(int rt, int l, int r, int pos, int v)
+void update(int rt, int l, int r, int pos, int val)
 {
-    if (l == r) {T[rt] = v; return;}
+    if (l == r) {T[rt] = val; return;}
     int mid = (l + r) >> 1;
-    if (pos <= mid) update(rt << 1, l, mid, pos, v);
-    else update(rt << 1 | 1, mid + 1, r, pos, v);
+    if (pos <= mid) update(rt << 1, l, mid, pos, val);
+    else update(rt << 1 | 1, mid + 1, r, pos, val);
     pushup(rt);
 }
 int query(int rt, int l, int r, int L, int R)
@@ -44,59 +68,33 @@ int query(int rt, int l, int r, int L, int R)
     int mid = (l + r) >> 1;
     int res = -INF;
     if (L <= mid) res = max(res, query(rt << 1, l, mid, L, R));
-    if (mid <  R) res = max(res, query(rt << 1 | 1, mid + 1, r, L, R));
+    if (mid < R)  res = max(res, query(rt << 1 | 1, mid + 1, r, L, R));
     return res;
 }
 
-void dfs1(int u, int fa, int depth)
-{
-    dep[u] = depth;
-    f[u] = fa;
-    sz[u] = 1;
-    son[u] = -1;
-    for (int i = head[u]; i; i = e[i].next)
-    {
-        int v = e[i].v;
-        if (v == f[u]) continue;
-        dfs1(v, u, depth + 1);
-        sz[u] += sz[v];
-        if (son[u] == -1 || sz[v] > sz[son[u]]) son[u] = v;
-    }
-}
-
-int cnt;
-void dfs2(int u, int tp)
-{
-    ++cnt;
-    top[u] = tp;
-    id[u] = cnt; rnk[cnt] = u;
-    if (son[u] > 0) dfs2(son[u], tp);
-    for (int i = head[u]; i; i = e[i].next)
-    {
-        int v = e[i].v;
-        if (v == f[u] || v == son[u]) continue;
-        dfs2(v, v);
-    }
-}
 int treequery(int x, int y)
 {
     int res = -INF;
-    int fx = top[x], fy = top[y];
-    while (fx != fy)
+    int topx = top[x], topy = top[y];
+    while (topx != topy)
     {
-        if (dep[fx] < dep[fy])
+        if (dep[topx] > dep[topy]) 
         {
-            swap(fx, fy);
-            swap(x, y);
+            res = max(res, query(1, 1, n, id[topx], id[x]));
+            x = father[topx]; topx = top[x];
         }
-        res = max(res, query(1, 1, n, id[fx], id[x]));
-        x = f[fx]; fx = top[x];
+        else
+        {
+            res = max(res, query(1, 1, n, id[topy], id[y]));
+            y = father[topy]; topy = top[y];
+        }
     }
     if (x == y) return res;
     if (dep[x] > dep[y]) swap(x, y);
     res = max(res, query(1, 1, n, id[son[x]], id[y]));
     return res;
 }
+
 int main()
 {
     int t; scanf("%d", &t);
@@ -111,22 +109,20 @@ int main()
         }
         int root = 1;
         dfs1(root, 0, 0);
-        cnt = 0; dfs2(root, root);
-        for (int i = 1; i < 2 * n; i += 2)
+        tim = 0; dfs2(root, root);
+        for (int i = 1; i <= tot; i += 2)
         {
-            if (dep[e[i].u] < dep[e[i].v]) swap(e[i].u, e[i].v);
-            val[id[e[i].u]] = e[i].w;
+            int u = e[i].u, v = e[i].v;
+            if (dep[u] < dep[v]) swap(e[i].u, e[i].v);
+            val[e[i].u] = e[i].w;
         }
         build(1, 1, n);
-        while (1)
+        char s[10];
+        while (scanf("%s", s) != EOF)
         {
-            char s[10]; scanf("%s", s);
             if (s[0] == 'D') break;
             int x, y; scanf("%d%d", &x, &y);
-            if (s[0] == 'Q') 
-            {
-                printf("%d\n", treequery(x, y));
-            }
+            if (s[0] == 'Q') printf("%d\n", treequery(x, y));
             else if (s[0] == 'C')
             {
                 int u = e[2 * x - 1].u;
