bb4ab4f9797405bfe79b1d0d452d61b18d060fc8
foolishme <2324532557@qq.com>
2019-03-09 15:33:26 +0800

    test

---
+++ sol.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define LL long long
+#define MEM(x,y) memset(x,y,sizeof(x))
+#define MOD(x) ((x)%mod)
+#define mod 1000000007
+#define pb push_back
+#define STREAM_FAST ios::sync_with_stdio(false)
+
+int main()
+{
+	
+	return 0;
+}
