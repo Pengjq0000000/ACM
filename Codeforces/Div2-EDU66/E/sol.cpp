e49e8665d3d6012ee20c439cd65d4086b2f251b0
foolishme <2324532557@qq.com>
2019-06-07 19:16:30 +0800

    2019.6.7

--- Codeforces/Div2-EDU66/E/sol.cpp
+++ Codeforces/Div2-EDU66/E/sol.cpp
@@ -19,12 +19,11 @@ int main()
 	for (int i = 1; i <= n; i++) 
 	{
 		scanf("%d%d", &seg[i].fir, &seg[i].sec);
-		seg[i].fir++; seg[i].sec++;
 		mx = max(seg[i].sec, mx);
 	}
 	sort(seg + 1, seg + 1 + n);
 	int now = 1, mxr = -1;
-	for (int i = 1; i <= mx; i++)
+	for (int i = 0; i <= mx; i++)
 	{
 		while (now <= n && seg[now].fir == i) 
 		{
@@ -33,30 +32,26 @@ int main()
 		}
 		if (mxr <= i) mxr = -1;
 		rg[i][0] = mxr;
-		//printf("%d ", rg[i][0]);
 	}
-	//puts("");
 	for (int j = 1; j <= 20; j++)
-		for (int i = 1; i <= mx; i++)	
+		for (int i = 0; i <= mx; i++)	
 		{
 			rg[i][j] = rg[i][j - 1] == -1 ? -1 : rg[rg[i][j - 1]][j - 1];
 		}
 	while (m--)
 	{
-		int x, y; scanf("%d%d", &x, &y); x++; y++;
+		int x, y; scanf("%d%d", &x, &y);
 		int ans = 0;
 		for (int j = 20; j >= 0; --j)
 		{
 			int tmpy = rg[x][j];
-			if (tmpy == -1) continue;
-			if (tmpy < y) 
+			if (tmpy != -1 && tmpy < y) 
 			{
 				ans += (1 << j);
 				x = tmpy;
 			}
 		}
-		if (rg[x][0] == -1) puts("-1");
-		else if (rg[x][0] >= y) printf("%d\n", ans + 1);
+		if (rg[x][0] >= y) printf("%d\n", ans + 1);
 		else puts("-1");
 	}
 	return 0;
