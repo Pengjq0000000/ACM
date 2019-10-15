a58d395890b01ce3c3b391f69438614fa44e89cf
FoolishMe <2324532557@qq.com>
2019-10-15 21:41:35 +0800

    2019.1015

--- 0ICPC-Template/DataStructures/LCApathcross.cpp
+++ 0ICPC-Template/DataStructures/LCApathcross.cpp
@@ -1,30 +1,41 @@
-// 欧拉序+ST表nlogn预处理 O(1)回答LCA
-// 利用4对点的LCA判断两条路径的交集
-// 路径交集可合并 可以用线段树维护 注意查询的初始值
+#include<bits/stdc++.h>
+#define pii pair<int, int>
+#define fir first
+#define sec second
+#define ll long long
+using namespace std;
+int read() 
+{
+    int x = 0, f = 1;
+    char c = getchar();
+    while (c < '0' || c > '9') f = (c == '-') ? -1 : f, c = getchar();
+    while (c >= '0' && c <= '9') x = x * 10 + c - '0', c = getchar();
+    return x * f;
+}
 const int N = 5e5 + 7;
 const int M = 20;
 vector<pii>G[N];
-int ST[N << 1][M], elr[N << 1], dep[N << 1], in[N];
+int ST[N << 1][M], elr[N << 1], dep[N], in[N];
 ll dist[N];
 int cnt = 0;
-void dfs(int u, int fa, int d)
+void dfs(int u, int fa)
 {
-    elr[++cnt] = u; dep[cnt] = d; in[u] = cnt;
+    elr[++cnt] = u; in[u] = cnt;
     for (pii edge : G[u])
     {
         int v = edge.fir, w = edge.sec;
         if (v == fa) continue;
-        dist[v] = dist[u] + w;
-        dfs(v, u, d + 1);
-        elr[++cnt] = u; dep[cnt] = d;
+        dist[v] = dist[u] + w; dep[v] = dep[u] + 1;
+        dfs(v, u);
+        elr[++cnt] = u;
     } 
 }
 inline int cal(int x, int y) {return dep[x] < dep[y] ? x : y;}
 void init(int rt)
 {
-    cnt = 0; dist[rt] = 0;
-    dfs(rt, 0, 1);
-    for (int i = 1; i <= cnt; i++) ST[i][0] = i;
+    cnt = 0; dist[rt] = 0; dep[rt] = 1;
+    dfs(rt, 0);
+    for (int i = 1; i <= cnt; i++) ST[i][0] = elr[i];
     for (int j = 1; j < 20; j++)
         for (int i = 1; i + (1 << j) - 1 <= cnt; i++)
             ST[i][j] = cal(ST[i][j - 1], ST[i + (1 << (j - 1))][j - 1]);
@@ -34,11 +45,13 @@ inline int LCA(int x, int y)
     int l = in[x], r = in[y];
     if (l > r) swap(l, r);
     int k = log2(r - l + 1);
-    return elr[cal(ST[l][k], ST[r - (1 << k) + 1][k])];
+    return cal(ST[l][k], ST[r - (1 << k) + 1][k]);
 }
 inline ll getdist(int u, int v){ return dist[u] + dist[v] - dist[LCA(u, v)] * 2;}
+#define lson (rt << 1)
+#define rson (rt << 1 | 1)
 struct node{int u, v; ll w;}path[N], T[N << 2];
-int p[4]; bool cmp(int x, int y) {return dep[in[x]] > dep[in[y]];}
+int p[4]; bool cmp(const int &x, const int &y)  {return dep[x] > dep[y];}
 node merge(node &x, node &y)
 {
     if (x.u == y.u && x.v == y.v) return x;
@@ -49,10 +62,9 @@ node merge(node &x, node &y)
     p[3] = LCA(x.v, y.v);
     sort(p, p + 4, cmp);
     int u = p[0], v = p[1]; 
-    if (u == v && (dep[in[u]] < dep[in[LCA(x.u, x.v)]] || dep[in[v]] < dep[in[LCA(y.u, y.v)]])) return {0, 0, 0};
+    if (u == v && (dep[u] < dep[LCA(x.u, x.v)] || dep[v] < dep[LCA(y.u, y.v)])) return {0, 0, 0};
     else return {u, v, getdist(u, v)};
 }
-
 void pushup(int rt) {T[rt] = merge(T[lson], T[rson]);}
 void build(int rt, int l, int r)
 {
@@ -77,4 +89,27 @@ node query(int rt, int l, int r, int L, int R)
     if (resl.w == -1) return resr;
     if (resr.w == -1) return resl;
     return merge(resl, resr);
+}
+int main()
+{
+    int n; scanf("%d", &n);
+    for (int i = 1, u, v, w; i < n; i++)
+    {
+        u = read(); v = read(); w = read();
+        G[u].push_back({v, w});
+        G[v].push_back({u, w});
+    }
+    init(1);
+    int m; scanf("%d", &m);
+    for (int i = 1; i <= m; i++) path[i].u = read(), path[i].v = read();
+    build(1, 1, m);
+    int q; scanf("%d", &q);
+    while (q--)
+    {
+        int l = read(), r = read(); 
+        node res = query(1, 1, m, l, r);
+        printf("%lld\n", res.w);
+    }
+
+    return 0;
 }
--- 51Nod/1830-树上路径交集/sol.cpp
+++ 51Nod/1830-树上路径交集/sol.cpp
@@ -15,27 +15,27 @@ int read()
 const int N = 5e5 + 7;
 const int M = 20;
 vector<pii>G[N];
-int ST[N << 1][M], elr[N << 1], dep[N << 1], in[N];
+int ST[N << 1][M], elr[N << 1], dep[N], in[N];
 ll dist[N];
 int cnt = 0;
-void dfs(int u, int fa, int d)
+void dfs(int u, int fa)
 {
-    elr[++cnt] = u; dep[cnt] = d; in[u] = cnt;
+    elr[++cnt] = u; in[u] = cnt;
     for (pii edge : G[u])
     {
         int v = edge.fir, w = edge.sec;
         if (v == fa) continue;
-        dist[v] = dist[u] + w;
-        dfs(v, u, d + 1);
-        elr[++cnt] = u; dep[cnt] = d;
+        dist[v] = dist[u] + w; dep[v] = dep[u] + 1;
+        dfs(v, u);
+        elr[++cnt] = u;
     } 
 }
 inline int cal(int x, int y) {return dep[x] < dep[y] ? x : y;}
 void init(int rt)
 {
-    cnt = 0; dist[rt] = 0;
-    dfs(rt, 0, 1);
-    for (int i = 1; i <= cnt; i++) ST[i][0] = i;
+    cnt = 0; dist[rt] = 0; dep[rt] = 1;
+    dfs(rt, 0);
+    for (int i = 1; i <= cnt; i++) ST[i][0] = elr[i];
     for (int j = 1; j < 20; j++)
         for (int i = 1; i + (1 << j) - 1 <= cnt; i++)
             ST[i][j] = cal(ST[i][j - 1], ST[i + (1 << (j - 1))][j - 1]);
@@ -45,13 +45,13 @@ inline int LCA(int x, int y)
     int l = in[x], r = in[y];
     if (l > r) swap(l, r);
     int k = log2(r - l + 1);
-    return elr[cal(ST[l][k], ST[r - (1 << k) + 1][k])];
+    return cal(ST[l][k], ST[r - (1 << k) + 1][k]);
 }
 inline ll getdist(int u, int v){ return dist[u] + dist[v] - dist[LCA(u, v)] * 2;}
 #define lson (rt << 1)
 #define rson (rt << 1 | 1)
 struct node{int u, v; ll w;}path[N], T[N << 2];
-int p[4]; bool cmp(int x, int y) {return dep[in[x]] > dep[in[y]];}
+int p[4]; bool cmp(const int &x, const int &y)  {return dep[x] > dep[y];}
 node merge(node &x, node &y)
 {
     if (x.u == y.u && x.v == y.v) return x;
@@ -62,7 +62,7 @@ node merge(node &x, node &y)
     p[3] = LCA(x.v, y.v);
     sort(p, p + 4, cmp);
     int u = p[0], v = p[1]; 
-    if (u == v && (dep[in[u]] < dep[in[LCA(x.u, x.v)]] || dep[in[v]] < dep[in[LCA(y.u, y.v)]])) return {0, 0, 0};
+    if (u == v && (dep[u] < dep[LCA(x.u, x.v)] || dep[v] < dep[LCA(y.u, y.v)])) return {0, 0, 0};
     else return {u, v, getdist(u, v)};
 }
 void pushup(int rt) {T[rt] = merge(T[lson], T[rson]);}
@@ -102,7 +102,6 @@ int main()
     init(1);
     int m; scanf("%d", &m);
     for (int i = 1; i <= m; i++) path[i].u = read(), path[i].v = read();
-   
     build(1, 1, m);
     int q; scanf("%d", &q);
     while (q--)
